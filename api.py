33ce89a5352d4988343682f17046bb6342090542
manish2409 <mgupta@ekomerz.com>
2015-08-14 12:22:47 +0530

    API

--- api.py
+++ api.py
@@ -14,28 +14,25 @@ class FlipkartAPI:
 			'Content-type': 'application/json',
 		})
 		return session
-    
+	
 	def search_orders(self):
 		url = "https://api.flipkart.net/sellers/orders/search"
-		filter = {"filter": {"states": ["PACKED","APPROVED","READY_TO_DISPATCH","CANCELLED"],},}
-		response = self.session.post(url, data=json.dumps(filter))
-		response_json = response.json()
-		orders = []
-		orders.append(response_json)
-		while response_json.get('hasMore') == True:
-			response = self.session.get('https://api.flipkart.net/sellers{0}'.format(response_json['nextPageUrl']))
-			response_json = response.json()
-			orders.append(response_json)
-		else:
-			print "All Orders Fetched"
-		return orders
-        
-        
+		filter = {"filter": {"states": ["APPROVED",],},}
+		return self.session.post(url, data=json.dumps(filter))
+		orderList = []
+		resp_json = response.json()
+		orderList.append(resp_json["orderItems"])
+		while resp_json.get('hasMore') == True:
+			response = self.session.get('https://api.flipkart.net/sellers{0}'.format(resp_json['nextPageUrl']))
+			resp_json = response.json()
+			orderList.append(resp_json["orderItems"])
+		return orderList
+	
 	def get_listing(self, skuid):
 		url = "https://api.flipkart.net/sellers/skus/%s/listings" % skuid
 		return self.session.get(url)
 	
-# TO-DO post_listing
+	''' TO-DO post_listing '''
 	def post_listing(self, skuid, attributes):
 		url = "https://api.flipkart.net/sellers/skus/%s/listings" % skuid
 		return self.session.post(url, attribute=attributes)
@@ -48,13 +45,55 @@ class FlipkartAPI:
 		url = "https://api.flipkart.net/sellers/orders/shipments"
 		payload = {'orderItemIds':','.join(orderItemIds)}
 		return self.session.get(url, params=payload)
-
-# TO-DO returns	
-	def returns(self, source=None, modifiedAfter=None, createdAfter=None):
-		url = "http://api.flipkart.net/returns"
-		payload = {'source':'',
-				   'modifiedAfter':'',
-				   'createdAfter':'',}
-		return self.session.get(url, params=payload)
-		
 	
+	''' TO_DO returns '''
+	def returns(self):
+		url = "http://api.flipkart.net/returns"
+		payload = {'source':['customer_return','courier_return'],
+				   'modifiedAfter':'2015-06-30',}
+		return self.session.get(url, params=payload)
+	
+	''' TO-DO bulk_listing'''
+	def bulk_listing(self, skuids):
+		url = "https://api.flipkart.net/sellers/skus/listings/bulk"
+		payload = {'listings':[]}
+	
+	def fetch_labels(self, orderItemIds):
+		url = "https://api.flipkart.net/sellers/orders/labels"
+		headers = {'Accept': 'application/octet-stream'}
+		payload = {'orderItemId':','.join(orderItemIds)}
+		return self.session.get(url, params=payload, headers=headers, stream=True)
+	
+	def get_orders_by_orderItemIds(self, orderItemIds):
+		url = "https://api.flipkart.net/sellers/orders/"
+		payload = {'orderItemIds':','.join(orderItemIds)}
+		return self.session.get(url, params=payload)
+	
+	def label_request(self, labelRequestId):
+		url = "https://api.flipkart.net/sellers/orders/labelRequest/%s" % labelRequestId
+		return self.session.get(url)
+	
+	''' TO-DO cancel_orders '''
+	def cancel_orders(self, orderItemId, reason=None):
+		url = "https://api.flipkart.net/sellers/orders/cancel"
+		payload = [{'orderItemId':orderItemId,
+					'reason':reason},]
+		return self.session.post(url, data = json.dumps(payload))
+	
+	''' TO-DO dispatch_orders '''
+	def dispatch_orders(self, orderItemId, quantity):
+		url = "https://api.flipkart.net/sellers/orders/dispatch"
+		payload = {"orderItems": [{"orderItemId": orderItemId,
+					"quantity": quantity},]}
+		return self.session.post(url, data =json.dumps(payload))
+	
+	def pack_orders(self, orderItemId, invoiceDate, invoiceNumber, tax, subOrderItemId = None, subInvoiceDate = None, subTax = None):
+		url = "https://api.flipkart.net/sellers/orders/labels"
+		payload = [{"orderItemId": orderItemId,
+					"serialNumbers": [],
+					"invoiceDate": invoiceDate,
+					"invoiceNumber": invoiceNumber,
+					"tax": tax,
+					"subItems": []
+					}]
+		return self.session.post(url, data=json.dumps(payload))

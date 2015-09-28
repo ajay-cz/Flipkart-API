07ef6792928e9f8e4abff3c953ac4e8718356e99
manish2409 <mgupta@ekomerz.com>
2015-09-28 12:29:01 +0530

    New Methods
    new methods implemented

--- api.py
+++ api.py
@@ -3,96 +3,236 @@ import json
 
 
 class FlipkartAPI:
-	def __init__(self, token):
+	def __init__(self, token, sandbox=False):
+
+		'''Initialize the FlipkartAPI Class with token we got from Authentication class.		
+		'''
+
 		self.token = token
 		self.session = self.get_session()
-    
+		self.sandbox = sandbox
+
+
+
+	
+
 	def get_session(self):
+
+		''' Create a session to GET or POST long data sequences. 
+		'''
+		
 		session = requests.Session()
 		session.headers.update({
 			'Authorization': 'Bearer %s' % self.token,
 			'Content-type': 'application/json',
 		})
 		return session
+
+
+
+
 	
-	def search_orders(self):
-		url = "https://api.flipkart.net/sellers/orders/search"
-		filter = {"filter": {"states": ["APPROVED",],},}
-		return self.session.post(url, data=json.dumps(filter))
-		orderList = []
+	def search_orders(self, order_states):
+
+		'''
+		Method search_orders fetch all the orders which are not completed. We have to pass the status of Orders as a list of strings.
+		Status: ["APPROVED", "CANCELLED", "READY_TO_DISPATCH", "PACKED"]
+		'''
+
+		if self.sandbox == True:
+			url = "https://sandbox-api.flipkart.net/sellers/orders/search"
+			url1 = 'https://sandbox-api.flipkart.net/sellers{0}'
+		else:
+			url = "https://api.flipkart.net/sellers/orders/search"
+			url1 = 'https://api.flipkart.net/sellers{0}'
+		filter = {"filter": {"states": order_states},}
+		response = self.session.post(url, data=json.dumps(filter))
 		resp_json = response.json()
-		orderList.append(resp_json["orderItems"])
+		data_string = resp_json
 		while resp_json.get('hasMore') == True:
-			response = self.session.get('https://api.flipkart.net/sellers{0}'.format(resp_json['nextPageUrl']))
+			response = self.session.get(url1.format(resp_json['nextPageUrl']))
 			resp_json = response.json()
-			orderList.append(resp_json["orderItems"])
-		return orderList
+			data_string.update(resp_json)
+		return data_string
+
+
+
+
 	
 	def get_listing(self, skuid):
-		url = "https://api.flipkart.net/sellers/skus/%s/listings" % skuid
+		if self.sandbox == True:
+			url = "https://sandbox-api.flipkart.net/sellers/skus/%s/listings" % skuid
+		else:
+			url = "https://api.flipkart.net/sellers/skus/%s/listings" % skuid
 		return self.session.get(url)
+
+
+
+
 	
-	''' TO-DO post_listing '''
-	def post_listing(self, skuid, attributes):
-		url = "https://api.flipkart.net/sellers/skus/%s/listings" % skuid
-		return self.session.post(url, attribute=attributes)
+	def post_listing(self, sku_id, listing_id, mrp, selling_price, status, nsc, zsc, lsc, sla, stock):
+		if self.sandbox == True:
+			url = "https://sandbox-api.flipkart.net/sellers/skus/%s/listings" % sku_id
+		else:
+			url = "https://api.flipkart.net/sellers/skus/%s/listings" % sku_id
+		payload = {"skuId": sku_id,
+					"listingId": listing_id,
+					"attributeValues": {
+						"mrp": mrp,
+						"selling_price": selling_price,
+						"listing_status": status,
+						"fulfilled_by": "seller",
+						"national_shipping_charge": nsc,
+						"zonal_shipping_charge": zsc,
+						"local_shipping_charge": lsc,
+						"procurement_sla": sla,
+						"stock_count": stock
+						}
+					}
+		return self.session.post(url, data = json.dumps(payload))
 	
 	def get_order_by_orderItemId(self, orderItemId):
-		url = "https://api.flipkart.net/sellers/orders/%s" % orderItemId
+		if self.sandbox == True:
+			url = "https://sandbox-api.flipkart.net/sellers/orders/%s" % orderItemId
+		else:
+			url = "https://api.flipkart.net/sellers/orders/%s" % orderItemId
 		return self.session.get(url)
+
+
+
+
 	
 	def shipment(self, orderItemIds):
-		url = "https://api.flipkart.net/sellers/orders/shipments"
+		if self.sandbox == True:
+			url = "https://sandbox-api.flipkart.net/sellers/orders/shipments"
+		else:
+			url = "https://api.flipkart.net/sellers/orders/shipments"
 		payload = {'orderItemIds':','.join(orderItemIds)}
 		return self.session.get(url, params=payload)
+
+
+
+
 	
 	''' TO_DO returns '''
 	def returns(self):
-		url = "http://api.flipkart.net/returns"
+		if self.sandbox == True:
+			url = "http://sandbox-api.flipkart.net/returns"
+		else:
+			url = "http://api.flipkart.net/returns"
 		payload = {'source':['customer_return','courier_return'],
 				   'modifiedAfter':'2015-06-30',}
 		return self.session.get(url, params=payload)
+
+
+
 	
-	''' TO-DO bulk_listing'''
-	def bulk_listing(self, skuids):
-		url = "https://api.flipkart.net/sellers/skus/listings/bulk"
+	def bulk_listing(self,listing_ids, sku_ids, fsns, mrps, selling_prices, status, nscs, zscs, lscs, slas, stocks):
+		if self.sandbox == True:
+			url = "https://sandbox-api.flipkart.net/sellers/skus/listings/bulk"
+		else:
+			url = "https://api.flipkart.net/sellers/skus/listings/bulk"
 		payload = {'listings':[]}
+		for lid, sku, fsn, mrp, sp, stts, nsc, zsc, lsc, sla, stck in zip(listing_ids, sku_ids, fsns, mrps, selling_prices, status, nscs, zscs, lscs, slas, stocks):
+			payload['listings'].append({"listingId": lid,
+										"skuId": sku,
+										"fsn": fsn,
+										"attributeValues": {
+											"mrp": mrp,
+											"selling_price": sp,
+											"listing_status": stts,
+											"fulfilled_by": "seller",
+											"national_shipping_charge": nsc,
+											"zonal_shipping_charge": zsc,
+											"local_shipping_charge": lsc,
+											"procurement_sla": sla,
+											"stock_count": stck}
+										})
+		return self.session.post(url, data = json.dumps(payload))
+
+
+
+
 	
 	def fetch_labels(self, orderItemIds):
-		url = "https://api.flipkart.net/sellers/orders/labels"
+
+		'''
+		This method fetch shipping labels for those orderItemIds which have been packed.
+		'''
+
+		if self.sandbox == True:
+			url = "https://sandbox-api.flipkart.net/sellers/orders/labels"
+		else:
+			url = "https://api.flipkart.net/sellers/orders/labels"
 		headers = {'Accept': 'application/octet-stream'}
 		payload = {'orderItemId':','.join(orderItemIds)}
 		return self.session.get(url, params=payload, headers=headers, stream=True)
+
+
+
+
 	
 	def get_orders_by_orderItemIds(self, orderItemIds):
-		url = "https://api.flipkart.net/sellers/orders/"
+		if self.sandbox == True:
+			url = "https://sandbox-api.flipkart.net/sellers/orders/"
+		else:
+			url = "https://api.flipkart.net/sellers/orders/"
 		payload = {'orderItemIds':','.join(orderItemIds)}
 		return self.session.get(url, params=payload)
+
+
+
+
 	
 	def label_request(self, labelRequestId):
-		url = "https://api.flipkart.net/sellers/orders/labelRequest/%s" % labelRequestId
+		if self.sandbox == True:
+			url = "https://sandbox-api.flipkart.net/sellers/orders/labelRequest/%s" % labelRequestId
+		else:
+			url = "https://api.flipkart.net/sellers/orders/labelRequest/%s" % labelRequestId
 		return self.session.get(url)
-	
-	''' TO-DO cancel_orders '''
-	def cancel_orders(self, orderItemId, reason=None):
-		url = "https://api.flipkart.net/sellers/orders/cancel"
-		payload = [{'orderItemId':orderItemId,
-					'reason':reason},]
+
+
+
+
+
+
+	def cancel_orders(self, order_item_id_list):
+		if self.sandbox == True:
+			url = "https://sandbox-api.flipkart.net/sellers/orders/cancel"
+		else:
+			url = "https://api.flipkart.net/sellers/orders/cancel"
+		payload = []
+		for order_item_id in order_item_id_list:
+			payload.append({'orderItemId':order_item_id,
+							 'reason':'Cancelled By Seller'})
 		return self.session.post(url, data = json.dumps(payload))
+
+
+
 	
-	''' TO-DO dispatch_orders '''
 	def dispatch_orders(self, orderItemId, quantity):
-		url = "https://api.flipkart.net/sellers/orders/dispatch"
+		if self.sandbox == True:
+			url = "https://sandbox-api.flipkart.net/sellers/orders/dispatch"
+		else:
+			url = "https://api.flipkart.net/sellers/orders/dispatch"
 		payload = {"orderItems": [{"orderItemId": orderItemId,
 					"quantity": quantity},]}
 		return self.session.post(url, data =json.dumps(payload))
+
+
+
+
 	
-	def pack_orders(self, orderItemId, invoiceDate, invoiceNumber, tax, subOrderItemId = None, subInvoiceDate = None, subTax = None):
-		url = "https://api.flipkart.net/sellers/orders/labels"
-		payload = [{"orderItemId": orderItemId,
+	def pack_orders(self, order_item_id, invoice_date, invoice_number, tax, sub_order_item_id = None, sub_invoice_date = None, sub_tax = None):
+		if self.sandbox == True:
+			url = "https://sandbox-api.flipkart.net/sellers/orders/labels"
+		else:
+			url = "https://api.flipkart.net/sellers/orders/labels"
+
+		payload = [{"orderItemId": order_item_id,
 					"serialNumbers": [],
-					"invoiceDate": invoiceDate,
-					"invoiceNumber": invoiceNumber,
+					"invoiceDate": invoice_date,
+					"invoiceNumber": invoice_number,
 					"tax": tax,
 					"subItems": []
 					}]
--- auth.py
+++ auth.py
@@ -1,11 +1,28 @@
 import requests
 
 class Authentication:
-    def __init__(self, app_id, app_secret):
-        self.app_id = app_id
-        self.app_secret = app_secret
-        self.url = "https://api.flipkart.net/oauth-service/oauth/token"
+    def __init__(self, application_id, application_secret, sandbox = False):
+
+        '''
+
+        Initialize all the parametres.
+        application_id and application_secret as provided by flipkart. Read the Flipkart APi for how to get your application_id and application_secret.
+
+        '''
+
+        self.application_id = application_id
+        self.application_secret = application_secret
+        self.sandbox = sandbox
+        if self.sandbox == True:
+        	self.url = "https://sandbox-api.flipkart.net/oauth-service/oauth/token"
+        else:
+            self.url = "https://api.flipkart.net/oauth-service/oauth/token"
     
     def get_access_token(self):
+
+        '''
+        Send a GET REQUEST to fetch an access token.
+        '''
+
         data = {'grant_type':'client_credentials','scope':'Seller_Api'}
-        return requests.get(self.url, params = data, auth = (self.app_id, self.app_secret))
+        return requests.get(self.url, params = data, auth = (self.application_id, self.application_secret))
